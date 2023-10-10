3e19d106a99c03b0935d464630575c437786fddf
Катя <ek8512@yandex.ru>
2023-10-10 22:22:40 +0300

    create 1t part of diplom

---
+++ main-service/src/main/java/ru/practicum/EWMMainServiceApp.java
@@ -0,0 +1,13 @@
+package ru.practicum;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class EWMMainServiceApp {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EWMMainServiceApp.class, args);
+    }
+}
---
+++ stats/stats-server/src/main/java/ru/practicum/EWMStatsServiceApp.java
@@ -0,0 +1,13 @@
+package ru.practicum;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class EWMStatsServiceApp {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EWMStatsServiceApp.class, args);
+    }
+}
