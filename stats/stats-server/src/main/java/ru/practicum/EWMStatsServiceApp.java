051cf92b34b241afb6a028b15f5b135db927844d
Катя <ek8512@yandex.ru>
2023-10-12 21:19:46 +0300

    delete whitespace

--- stats/stats-server/src/main/java/ru/practicum/EWMStatsServiceApp.java
+++ stats/stats-server/src/main/java/ru/practicum/EWMStatsServiceApp.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EWMStatsServiceApp {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         SpringApplication.run(EWMStatsServiceApp.class, args);
     }
 }
