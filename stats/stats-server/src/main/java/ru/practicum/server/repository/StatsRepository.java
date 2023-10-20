bc4d010fae988672b97d509fac5bd3f45e243880
Катя <ek8512@yandex.ru>
2023-10-20 19:05:25 +0300

    pom

--- stats/stats-server/src/main/java/ru/practicum/server/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/server/repository/StatsRepository.java
@@ -11,25 +11,25 @@ import java.util.List;
 
 @Repository
 public interface StatsRepository extends JpaRepository<Stat, Long> {
-    @Query(value = "SELECT NEW ru.practicum.HitResponseDto(s.app, s.uri, COUNT(s.ip)) " +
+    @Query(value = "SELECT NEW ru.practicum.statsDto.HitResponseDto(s.app, s.uri, COUNT(s.ip)) " +
             "FROM Stat AS s " +
             "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri ORDER BY COUNT(s.ip) DESC")
     List<HitResponseDto> getStatsUri(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query(value = "SELECT NEW ru.practicum.HitResponseDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
+    @Query(value = "SELECT NEW ru.practicum.statsDto.HitResponseDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stat AS s " +
             "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<HitResponseDto> getStatsUriUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query(value = "SELECT NEW ru.practicum.HitResponseDto(s.app, s.uri, COUNT(s.ip)) " +
+    @Query(value = "SELECT NEW ru.practicum.statsDto.HitResponseDto(s.app, s.uri, COUNT(s.ip)) " +
             "FROM Stat AS s " +
             "WHERE s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri ORDER BY COUNT(s.ip) DESC")
     List<HitResponseDto> getStats(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT NEW ru.practicum.HitResponseDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
+    @Query(value = "SELECT NEW ru.practicum.statsDto.HitResponseDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stat AS s " +
             "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
             "GROUP BY s.app, s.uri ORDER BY count(DISTINCT s.ip) DESC")
