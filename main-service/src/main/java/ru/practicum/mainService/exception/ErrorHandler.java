01b4652b5e7d44abe890d1375dc08084cc53cb49
Катя <ek8512@yandex.ru>
2023-10-22 18:03:47 +0300

    added valid

--- main-service/src/main/java/ru/practicum/mainService/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/mainService/exception/ErrorHandler.java
@@ -1,29 +1,23 @@
 package ru.practicum.mainService.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler({ValidationException.class,
+            IllegalArgumentException.class,
+            BadRequestException.class,
+            ConflictException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIllegalArgument(final IllegalArgumentException e) {
-        return new ErrorResponse(String.format(e.getMessage()));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIllegalArgument(final BadRequestException e) {
-        return new ErrorResponse(String.format(e.getMessage()));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleEmailExistingException(final ConflictException exception) {
-        return new ErrorResponse(exception.getMessage());
+    public ErrorResponse handleValidationException(final Exception e) {
+        log.error(e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
---
+++ main-service/src/main/java/ru/practicum/mainService/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package ru.practicum.mainService.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
+
